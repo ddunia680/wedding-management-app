fc8ddd36ba8110bd67ae99947297189c0fb39ac1
ddunia680 <ddunia680@gmail.com>
2023-08-29 13:49:16 +0300

    second update today

--- src/Components/Header/header.js
+++ src/Components/Header/header.js
@@ -134,10 +134,10 @@ function Header(props) {
                         {t('programB')} <NewspaperIcon className='w-[1rem]'/>
                     </p>
                     { token ? <p className=' px-[1rem] py-[0.3rem] text-[13px] text-whitish dark:text-gray-500 flex justify-start'>
-                        {t('scanQRB')} <NewspaperIcon className='w-[1rem]'/>
+                        {t('scanQRB')} <NewspaperIcon className='w-[1rem]' onClick={() => navigate('/scanQRCode')}/>
                     </p> : null}
                     <p className='bg-white dark:bg-darkLighterBlue dark:text-white rounded-lg text-[13px] px-[1rem] py-[0.3rem] flex justify-start'
-                    onClick={() => navigate('/adminLog')}>
+                    onClick={() => navigate(token ? '/adminLog/manage' : '/adminLog')}>
                         {t('adminB')} <UserIcon className='text-specialPink w-[1rem]'/>
                     </p>
                     { token ? <p className=' px-[1rem] py-[0.3rem] text-[13px] text-red-700 flex justify-start' onClick={() => issueLogout()}>
